19068b73ea30e3b70379042e05fd4e36728b9616
ms-shakil <mdshakil8796@gmail.com>
2021-08-26 14:32:22 +0600

    just start this project

---
+++ player.py
@@ -0,0 +1,24 @@
+import math
+import random
+
+class Player:
+    def __init__(self,letter):
+        self.letter = letter
+
+    def get_move (self,game) :
+        pass
+
+class RandomComPlayer(Player):
+    def __init__(self,letter):
+        super.__init__(self,letter)
+
+    def get_move(self,game):
+        pass
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        pass 
+
