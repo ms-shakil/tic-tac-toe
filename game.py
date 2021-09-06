53725cfb4b3d555c1d98ecfb18460e7e14bf02cd
ms-shakil <mdshakil8796@gmail.com>
2021-09-06 14:10:40 +0600

    creat some function

--- game.py
+++ game.py
@@ -15,6 +15,40 @@ class TicTacToe:
     def available_move(self):
         move = [i for i,spot in enumerate(self.board) if spot == " "]
         return move
+    
+    def empty_squre(self):
+        return " " in self.board
 
-       
+    def Num_empty_squre(self):
+        return self.board.count(" ")    
 
+    def make_moves(self,squre,letter):
+        if self.board[squre] == " ":
+            self.board[squre] = letter
+            if self.winner(squre,letter):
+                self.currentwinner = letter
+            return True  
+        return False      
+
+    def winner(self):
+        pass        
+def Play(game,x_player,o_player,print_bord =True):
+    if print_bord:
+        game.printboard()
+    letter = "X"
+    while game.empty_squre():
+        if letter == "O":
+            squre = o_player.get_move(game)
+        else:
+            squre = x_player.get_move(game)    
+        
+        if game.make_move(squre,letter):
+            if print_bord:
+                print(letter + f"make a move to squre{squre}")
+                game.printboard()
+                print(" ")
+            
+            if game.currentwinner:
+                if print_bord:
+                    print(letter +"winner!")
+            letter = "O" if letter == "X" else "X"    
